a5634d527c05cd27fba252ffe084e32456e916ab
Roopam0070 <roopam0070@gmail.com>
2025-09-15 14:19:02 +0530

    Updated search functionality and UI

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,28 +5,46 @@ const HomePage = () => {
     const [data, setData]=useState([]);
     const [search, setSearch]=useState("");
     const [mssg,setmssg]=useState("");
+    const [loading, setLoading] = useState(false);
+
     const searchValue=(e)=>{
         setSearch(e.target.value);
     }
 
     const searchRecipe = async () => {
+        setLoading(true);          // start loading
+        setmssg("Fetching data..."); 
         if(search===""){
             setmssg("Please Enter something");
+            setData([])
         }
         else{
-            const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
-            const jsonData = await get.json();
-            setData(jsonData.meals)
-            setmssg("");
+            try {
+                const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+                const jsonData = await get.json();
+                setData(jsonData.meals)
+                setmssg("");
+            } catch (error) {
+                setmssg("Something went wrong!");
+            }
+            finally {
+                setLoading(false);       // stop loading in all cases
+            }
         }     
     }
 
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+          searchRecipe();
+        }
+      };
+
   return (
       <>
-          <h1 className='head'>FOOD RECIPE APP</h1>
+          <h1 className='head'>RECIPE FINDER</h1>
           <div className='container'>
               <div className='searchBar'>
-                  <input type="text" placeholder='Search Dish' onChange={searchValue} />
+                  <input type="text" placeholder='Search Dish' onChange={searchValue} onKeyDown={handleKeyDown} />
                   <button onClick={searchRecipe}>Search</button>
               </div>
               <h4 className='msg'>{mssg}</h4>
